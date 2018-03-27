f1d9bb6599efe5eb8a7d0aa9399d3c51a6d41636
sergey.cherkesov <sergey.cherkasov@devim.team>
2018-03-27 16:50:54 +0300

    fix response format

--- Controller/GatewayController.php
+++ Controller/GatewayController.php
@@ -28,36 +28,53 @@ class GatewayController implements ContainerAwareInterface
 
     public function options()
     {
-        $response = new Response(null, 204);
-        $response->headers->set('Access-Control-Allow-Headers', 'authorization,content-type,x-device-id');
-        $response->headers->set('Access-Control-Allow-Methods', 'POST,POST');
-//        $response->headers->set('Access-Control-Allow-Origin', 'https://dzp-test.devim.team');
-        $response->headers->set('Access-Control-Max-Age', '15');
-        return $response;
+//        $response = new Response(null, 204);
+//        $response->headers->set('Access-Control-Allow-Headers', 'authorization,content-type,x-device-id');
+//        $response->headers->set('Access-Control-Allow-Methods', 'POST,POST');
+////        $response->headers->set('Access-Control-Allow-Origin', 'https://dzp-test.devim.team');
+//        $response->headers->set('Access-Control-Max-Age', '15');
+        return new Response(null, 204);
     }
 
     public function handler(Request $request)
     {
-        $content = $request->getContent();
+        try {
+            $content = $request->getContent();
 
-        /** @var \ReflectionClass $classRefl */
-        /** @var \ReflectionMethod $methodRefl */
-        list($classRefl, $methodRefl) = $this->jsonRpcDiscovery->resolve($content);
-        $controller = $this->container->get($classRefl->getName());
+            /** @var \ReflectionClass $classRefl */
+            /** @var \ReflectionMethod $methodRefl */
+            list($classRefl, $methodRefl) = $this->jsonRpcDiscovery->resolve($content);
+            $controller = $this->container->get($classRefl->getName());
 
-        $parametersData = json_decode($content, true)['params'];
-        $resultInputParams = [];
+            $parametersData = json_decode($content, true)['params'];
+            $resultInputParams = [];
 
-        foreach ($methodRefl->getParameters() as $parameter) {
-            $resultInputParams[] = isset($parametersData[$parameter->getName()])
-                ? $parametersData[$parameter->getName()] : null;
+            foreach ($methodRefl->getParameters() as $parameter) {
+                $resultInputParams[] = isset($parametersData[$parameter->getName()])
+                    ? $parametersData[$parameter->getName()] : null;
+            }
+            $response = $methodRefl->invokeArgs($controller, $resultInputParams);
+
+            if (is_array($response)) {
+                $response = new JsonResponse([
+                    'jsonrpc' => '2.0',
+                    'result' => $response,
+                    'id' => rand(0, 100)
+                ]);
+            } elseif (is_object($response)) {
+                throw new \Exception('Unsupported response type - object!! Lost realization with JMS Serializer!!');
+            }
+
+            return $response;
+        } catch (\Exception $exception) {
+            return new JsonResponse([
+                'jsonrpc' => '2.0',
+                'error' => [
+                    'code' => -32601,
+                    'message' => $exception->getMessage(),
+                ],
+                'id' => rand(0, 100)
+            ]);
         }
-        $response = $methodRefl->invokeArgs($controller, $resultInputParams);
-
-        if (is_array($response)) {
-            $response = new JsonResponse($response);
-        }
-
-        return $response;
     }
 }
